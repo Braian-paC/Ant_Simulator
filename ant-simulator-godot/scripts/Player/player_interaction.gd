c5436aebfaa27ea20bd25a62d17bc1024b45a045
Braian-paC <braianbbg4@gmail.com>
2026-06-24 16:46:27 -0300

    KEY 'ESCAPE' to close the game

--- ant-simulator-godot/scripts/Player/player_interaction.gd
+++ ant-simulator-godot/scripts/Player/player_interaction.gd
@@ -4,8 +4,7 @@ var food_node = null
 @export var food_name = "StaticBody2D_Food"
 
 func handle(player):
-	if Input.is_action_just_pressed("interact"):
-
+	if Input.is_action_just_pressed("InteractItem"):
 		if food_node:
 			var pos = player.global_position
 			player.get_parent().add_child(food_node)
@@ -19,3 +18,6 @@ func handle(player):
 				if col.name == food_name:
 					food_node = col
 					food_node.get_parent().remove_child(food_node)
+
+	if Input.is_action_just_pressed("ExitGame"):
+		get_tree().quit()
