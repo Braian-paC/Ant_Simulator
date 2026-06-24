561f9860c52ed6a9782d446b76b55fbeda99df74
Braian-paC <braianbbg4@gmail.com>
2026-06-24 18:59:07 -0300

    Player Rotation

--- ant-simulator-godot/scripts/Player/player_movement.gd
+++ ant-simulator-godot/scripts/Player/player_movement.gd
@@ -2,6 +2,13 @@ extends Node
 
 @export var speed = 300
 
+
 func handle(player):
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	player.velocity = input_direction * speed
+	
+	if input_direction != Vector2.ZERO:
+		player.velocity = input_direction * speed
+		player.rotation = player.velocity.angle()
+	else:
+		player.velocity = player.velocity.move_toward(Vector2.ZERO, speed)
